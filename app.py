5e5d0925cc1f95b0414666b1b1213d0af3cc8c0d
Sasan Bahadaran <sasan.baha@gmail.com>
2015-08-03 00:58:41 -0400

    fixed issues and added calculation

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-# We need to import request to access the details of the POST request
-# and render_template, to render our templates (form and response)
-# we'll use url_for to get some URLs for the app on the templates
 from flask import Flask, render_template, request, url_for
 
-# Initialize the Flask application
 app = Flask(__name__)
 
 # Define a route for the default URL, which loads the form
@@ -11,33 +7,24 @@ app = Flask(__name__)
 def form():
     return render_template('form_submit.html')
 
-# Define a route for the action of the form, for example '/hello/'
-# We are also defining which type of requests this route is 
-# accepting: POST requests in this case
 @app.route('/hello/', methods=['POST'])
 def hello():
-    #name=request.form['yourname']
-    #email=request.form['youremail']
-    #food=request.form['food']
-    #medical=request.form['medical']
-    #housing=request.form['housing']
-    #recreation=request.form['recreation']
-    #apparel=request.form['apparel']
-    #education=request.form['education']
-    #transport=request.form['transport']
-    #other=request.form['other']
-    #a=array(food,medical,housing,recreation,apparel,transport,other)
-    #cpi=mycpi(a)
-    #print cpi
-    print "1"
-    
-def mycpi(arr):
-    for element in arr:
-      cat+=element
-      
-    cpi=cat/8
-    return cpi
+    food=request.form['food']
+    medical=request.form['medical']
+    housing=request.form['housing']
+    recreation=request.form['recreation']
+    apparel=request.form['apparel']
+    education=request.form['education']
+    transport=request.form['transport']
+    other=request.form['other']
+    arr = [food,medical,housing,recreation,apparel,education,transport,other]
+    mycpivalue=mycpi(arr)
+    return render_template('form_submit.html', food=food, medical=medical, housing=housing, recreation=recreation,
+    apparel=apparel, education=education, transport=transport, other=other, mycpi=mycpivalue)
 
-# Run the app :)
+def mycpi(values):
+    return sum(int(i) for i in values) / 8
+
 if __name__ == '__main__':
   app.run()
+
